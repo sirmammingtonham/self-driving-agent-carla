90c761bd7eff32fd8207fc8e10b811c4c26bd100
Mason Sklar <msklar2001@gmail.com>
2021-11-14 12:24:21 -0500

    small tweaks to testpilot.py
    actor destruction ping still timing out

--- testpilot.py
+++ testpilot.py
@@ -18,20 +18,21 @@ import random
 import time
 
 #enables a constant velocity on vehicle
-#returns True to break the game loop
+#returns False to break the game loop
 def moveVehicle(endTime, vehicle):
     if(int(time.time()) > endTime):
-        return True
+        return False
 
     vehicle.enable_constant_velocity(carla.Vector3D(17, 0, 0))
+    return True
 
 
 def main():
     actorList = []
-    print("scriptrunning?")
+    print("testpilot script started")
     try:
         #creates the client
-        client = carla.Client("localhost", 2000)
+        client = carla.Client('localhost', 2000)
         client.set_timeout(10.0)
 
         #retrieve the world and blueprint list from the server
@@ -47,16 +48,15 @@ def main():
         #spawns a vehicleBP actor at vehicleTransform
         vehicle = world.spawn_actor(vehicleBP, vehicleTransform)
         actorList.append(vehicle)
-        print("loaded vehicle %s" % vehicle.type_id)
+        print("loaded vehicle %s at location (%s, %s)" % (vehicle.type_id, vehicleTransform.location.x, vehicleTransform.location.y))
 
         #determine time to kill this script
         startTime = int(time.time())
-        endTime = startTime + 15
+        endTime = startTime + 5
 
         #game loop
-        while(True):
-            if(moveVehicle(endTime, vehicle)):
-                return
+        while(moveVehicle(endTime, vehicle)):
+            pass
 
     finally:
         print("destroying all actors")
